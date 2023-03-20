0f2285ef91e4a4cc45ce909c42104c15b63116e8
csoke.balazs <csoke.balazs@28-10>
2023-03-20 16:28:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Gyakjuhasz;
+using System.Text;
+
+List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+StreamReader sr = new("ub2017egyedi.txt", Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    versenyzok.Add(new Versenyzo(sr.ReadLine()));
+}
+sr.Close();
+
+Console.WriteLine($"1. feladat: {versenyzok.Count}");
---
+++ Versenyzo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakjuhasz
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int RajtSzam { get; set; }
+        public string Kategoria { get; set; }
+        public string Idoeredmeny { get; set; }
+        public int TavSzazalek { get; set; }
+
+        public Versenyzo(string fileEgySora)
+        {
+            string[] aFileSorainakDarabjai = fileEgySora.Split(';');
+            this.Nev = aFileSorainakDarabjai[0];
+            this.RajtSzam = int.Parse(aFileSorainakDarabjai[1]);
+            this.Kategoria = aFileSorainakDarabjai[2];
+            this.Idoeredmeny = aFileSorainakDarabjai[3];
+            this.TavSzazalek = int.Parse(aFileSorainakDarabjai[4]);
+        }
+    }
+}
